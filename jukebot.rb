12fde4f5f69b1eedfe13e0c3f2e8333f39e2073d
Eric Stiens <estiens@users.noreply.github.com>
2017-10-05 02:17:20 -0500

    some spotify integration

--- jukebot.rb
+++ jukebot.rb
@@ -4,12 +4,11 @@ Dotenv.load
 require 'slack-ruby-bot'
 require 'httparty'
 require 'pry'
-
-BASE_URL = "http://#{ENV['USERNAME']}:#{ENV['PASSWORD']}@24.7.192.65:5005".freeze
+require 'rspotify'
 
 class SonosApi
   include HTTParty
-  base_uri 'http://24.7.192.65:5005'
+  base_uri 'http://24.7.192.65:5005/'
 
   attr_accessor :sonos_room
 
@@ -18,8 +17,18 @@ class SonosApi
     @sonos_room = sonos_room
   end
 
+  def spotify_play(track_url, play_when = 'now')
+    url = URI.encode("/#{@sonos_room}/spotify/#{play_when}/#{track_url}")
+    self.class.get(url, @options)
+  end
+
   def say(text)
     url = URI.encode("/#{@sonos_room}/say/#{text}/50")
+    self.class.get(url, @options)
+  end
+
+  def change_volume(volume)
+    url = URI.encode("/#{@sonos_room}/volume/#{volume}")
     self.class.get(url, @options)
   end
 
@@ -35,9 +44,102 @@ class SonosApi
   end
 end
 
+class SpotifySearcher
+  attr_reader :last_search
+
+  def initialize
+    @last_search = nil
+  end
+
+  def find(query)
+    data = find_data(query)
+    data.nil? ? nil : data.uri
+  end
+
+  def find_data(query)
+    tracks = find_tracks(query)
+    !tracks.empty? ? tracks.first : nil
+  end
+
+  def find_tracks(query:, limit:)
+    limit ||= 3
+    RSpotify.authenticate(ENV['RSPOTIFY_TOKEN'], ENV['RSPOTIFY_PASSWORD'])
+    tracks = RSpotify::Track.search(query, limit: limit, market: 'US')
+    @last_search = tracks.sort_by { |track| -track.popularity }
+  end
+
+  def get_album_info(uri)
+    RSpotify.authenticate(ENV['RSPOTIFY_TOKEN'], ENV['RSPOTIFY_PASSWORD'])
+    uri = uri.gsub('spotify:album:', '')
+    RSpotify::Album.find(uri)
+  end
+end
+
 class JukeBot < SlackRubyBot::Bot
   def self.api
     @api ||= SonosApi.new
+  end
+
+  def self.spotify
+    @spotify ||= SpotifySearcher.new
+  end
+
+  match(/find ?(\d+)? music (.*)/i) do |client, data, match|
+    tracks = spotify.find_tracks(query: match[2], limit: match[1])
+    artist_array = []
+    tracks.each do |track|
+      artists = track.artists.map(&:name).join(',')
+      name = track.name
+      album = track.album.name
+      artist_array << { artists: artists, name: name, album: album }
+    end
+    response = "I found the following #{artist_array.length} songs."
+    artist_array.each_with_index do |artist, idx|
+      response += "#{idx + 1}: #{artist[:name]} by #{artist[:artists]} on #{artist[:album]}. "
+    end
+    client.say(text: response, channel: data.channel)
+  end
+
+  match(/play (.*)/i) do |client, data, match|
+    number = match[1].to_i.to_s == match[1]
+    if number
+      song_index = match[1].to_i - 1
+      uri = spotify.last_search[song_index].uri
+      api.spotify_play(uri)
+      preview_image = spotify.last_search[song_index].album.images.first['url']
+      response = "Alright, I'm now playing your request. #{preview_image}"
+      client.say(text: response, channel: data.channel)
+    end
+  end
+
+  match(/next (.*)/i) do |client, data, match|
+    number = match[1].to_i.to_s == match[1]
+    if number
+      song_index = match[1].to_i - 1
+      uri = spotify.last_search[song_index].uri
+      api.spotify_play(uri, 'next')
+      preview_image = spotify.last_search[song_index].album.images.first['url']
+      response = "Alright, I'm playing what you want next. #{preview_image}"
+      client.say(text: response, channel: data.channel)
+    end
+  end
+
+  match(/queue (.*)/i) do |client, data, match|
+    number = match[1].to_i.to_s == match[1]
+    if number
+      song_index = match[1].to_i - 1
+      uri = spotify.last_search[song_index].uri
+      api.spotify_play(uri, 'queue')
+      response = "Alright, I queued up #{spotify.last_search[song_index].name}"
+      client.say(text: response, channel: data.channel)
+    end
+  end
+
+  match(/volume (.*)/i) do |client, data, match|
+    volume = match[1]
+    api.change_volume(volume)
+    response = "Set the volume to #{volume} :mega:"
+    client.say(text: response, channel: data.channel)
   end
 
   match(/.*\schange\sroom\s(?:to )?(?<room>.*)$/i) do |client, data, match|
