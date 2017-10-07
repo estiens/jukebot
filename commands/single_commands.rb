2dfedcba3bf8e3e11e7b2f8f89ac43657f93d8d5
Eric Stiens <estiens@users.noreply.github.com>
2017-10-07 17:41:55 -0500

    all the docs

--- commands/single_commands.rb
+++ commands/single_commands.rb
@@ -1,8 +1,18 @@
 class JukeBot < SlackRubyBot::Bot
+  help do
+    title 'current room'
+    desc '`current_room` - tells you current room under control'
+  end
+
   command 'current room' do |client, data, _match|
     room = api.current_room
     response = "Current room under control is #{room}"
     client.say(text: response, channel: data.channel)
+  end
+
+  help do
+    title 'pause'
+    desc '`pause` - pauses music'
   end
 
   command 'pause' do |client, data, _match|
@@ -11,10 +21,20 @@ class JukeBot < SlackRubyBot::Bot
     client.say(text: response, channel: data.channel)
   end
 
+  help do
+    title 'resume'
+    desc '`resume` - resumes music'
+  end
+
   command 'resume' do |client, data, _match|
     api.play
     response = 'Here we go!'
     client.say(text: response, channel: data.channel)
+  end
+
+  help do
+    title 'mute'
+    desc '`mute` - mutes music'
   end
 
   command 'mute' do |client, data, _match|
@@ -23,10 +43,20 @@ class JukeBot < SlackRubyBot::Bot
     client.say(text: response, channel: data.channel)
   end
 
+  help do
+    title 'unmutes'
+    desc '`unmutes` - unmutess music'
+  end
+
   command 'unmute' do |client, data, _match|
     api.groupUnmute
     response = 'unmuted'
     client.say(text: response, channel: data.channel)
+  end
+
+  help do
+    title 'rooms'
+    desc '`rooms` - gives you a list of all sonos rooms and groups'
   end
 
   command 'rooms' do |client, data, _match|
@@ -38,10 +68,20 @@ class JukeBot < SlackRubyBot::Bot
     client.say(text: response, channel: data.channel)
   end
 
+  help do
+    title 'previous'
+    desc '`previous` - plays previous track'
+  end
+
   command 'previous' do |client, data, _match|
     api.previous
     response = 'Playing it again, Sam.'
     client.say(text: response, channel: data.channel)
+  end
+
+  help do
+    title 'next'
+    desc '`next` - plays next track'
   end
 
   command 'next' do |client, data, _match|
@@ -50,10 +90,20 @@ class JukeBot < SlackRubyBot::Bot
     client.say(text: response, channel: data.channel)
   end
 
+  help do
+    title 'lock'
+    desc '`lock` - locks volumes of all rooms'
+  end
+
   command 'lock' do |client, data, _match|
     api.lockvolumes
     response = 'Volumes Locked!'
     client.say(text: response, channel: data.channel)
+  end
+
+  help do
+    title 'unlock'
+    desc '`unlock` - unlocks volumes of all rooms'
   end
 
   command 'unlock' do |client, data, _match|
@@ -62,10 +112,21 @@ class JukeBot < SlackRubyBot::Bot
     client.say(text: response, channel: data.channel)
   end
 
+  help do
+    title 'clear queue'
+    desc '`clear queue` - clears the whole queue'
+    long desc 'Warning: stops current music.'
+  end
+
   command 'clear queue' do |client, data, _match|
     api.clearqueue
     response = 'Alright, queue is empty!'
     client.say(text: response, channel: data.channel)
+  end
+
+  help do
+    title 'linein'
+    desc '`linein` - Switches the current room/group to linein'
   end
 
   command 'linein' do |client, data, _match|
